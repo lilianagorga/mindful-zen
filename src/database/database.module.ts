2a35fbb76aabdf9cdfcac836daa8ca345b7f9f40
Liliana Gorga <gorgaliliana87@gmail.com>
2025-01-05 13:00:15 +0100

    trigger deploy

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -11,44 +11,34 @@ import { CustomNamingStrategy } from '../custom-naming-strategy';
       envFilePath:
         process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
     }),
-    ...(process.env.NODE_ENV === 'production'
-      ? []
-      : [
-          TypeOrmModule.forRootAsync({
-            useFactory: () => {
-              const isTestEnv = process.env.NODE_ENV === 'test';
-              const databaseName = isTestEnv
-                ? process.env.DB_TEST_NAME
-                : process.env.DB_NAME;
-              return {
-                type: 'postgres',
-                host: process.env.DB_HOST,
-                port: parseInt(process.env.DB_PORT, 10),
-                username: process.env.DB_USER,
-                password: process.env.DB_PASS,
-                database: databaseName,
-                autoLoadEntities: true,
-                synchronize: false,
-                namingStrategy: new CustomNamingStrategy(),
-              };
-            },
-          }),
-        ]),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        const isTestEnv = process.env.NODE_ENV === 'test';
+        const databaseName = isTestEnv
+          ? process.env.DB_TEST_NAME
+          : process.env.DB_NAME;
+        return {
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT, 10),
+          username: process.env.DB_USER,
+          password: process.env.DB_PASS,
+          database: databaseName,
+          autoLoadEntities: true,
+          synchronize: false,
+          namingStrategy: new CustomNamingStrategy(),
+        };
+      },
+    }),
   ],
   providers: [
     {
       provide: 'MONGO_CLIENT',
       useFactory: async () => {
-        if (process.env.NODE_ENV === 'production') {
-          const client = new MongoClient(process.env.MONGODB_URI);
-          await client.connect();
-          console.log('MongoDB connection established');
-          return client;
-        }
-        console.warn(
-          'MongoDB is not connected: Environment is not production.',
-        );
-        return null;
+        const client = new MongoClient(process.env.MONGO_URI);
+        await client.connect();
+        console.log('MongoDB connection established in all environments');
+        return client;
       },
     },
   ],
