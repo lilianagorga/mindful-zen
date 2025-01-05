baa05c5f231eab4e03484e842c9941dd7ca693b0
Liliana Gorga <gorgaliliana87@gmail.com>
2025-01-05 13:50:38 +0100

    again trigger deploy

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -12,37 +12,45 @@ import { CustomNamingStrategy } from '../custom-naming-strategy';
         process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
     }),
     ...(process.env.NODE_ENV === 'production'
-      ? []
-      : [
-          TypeOrmModule.forRootAsync({
-            useFactory: () => {
-              const isTestEnv = process.env.NODE_ENV === 'test';
-              const databaseName = isTestEnv
-                ? process.env.DB_TEST_NAME
-                : process.env.DB_NAME;
-              const config: TypeOrmModuleOptions = {
-                type: 'postgres',
-                host: process.env.DB_HOST,
-                port: parseInt(process.env.DB_PORT, 10),
-                username: process.env.DB_USER,
-                password: process.env.DB_PASS,
-                database: databaseName,
-                autoLoadEntities: true,
-                synchronize: false,
-                namingStrategy: new CustomNamingStrategy(),
-              };
-              console.log('TypeORM Configuration:', config);
+      ? (() => {
+          console.log('TypeORM is excluded in production.');
+          return [];
+        })()
+      : (() => {
+          console.log(
+            'TypeORM is included in development or test environment.',
+          );
+          return [
+            TypeOrmModule.forRootAsync({
+              useFactory: () => {
+                const isTestEnv = process.env.NODE_ENV === 'test';
+                const databaseName = isTestEnv
+                  ? process.env.DB_TEST_NAME
+                  : process.env.DB_NAME;
+                const config: TypeOrmModuleOptions = {
+                  type: 'postgres',
+                  host: process.env.DB_HOST,
+                  port: parseInt(process.env.DB_PORT, 10),
+                  username: process.env.DB_USER,
+                  password: process.env.DB_PASS,
+                  database: databaseName,
+                  autoLoadEntities: true,
+                  synchronize: false,
+                  namingStrategy: new CustomNamingStrategy(),
+                };
+                console.log('TypeORM Configuration:', config);
 
-              return config;
-            },
-          }),
-        ]),
+                return config;
+              },
+            }),
+          ];
+        })()),
   ],
   providers: [
     {
       provide: 'MONGO_CLIENT',
       useFactory: async () => {
-        const client = new MongoClient(process.env.MONGO_URI);
+        const client = new MongoClient(process.env.MONGODB_URI);
         await client.connect();
         console.log('MongoDB connection established in all environments');
         return client;
